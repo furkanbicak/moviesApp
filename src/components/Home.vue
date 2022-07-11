9bc0ec0096502d8ea813450d15befbb192f0d162
afurkanbicak <afurkanbicak@gmail.com>
2022-07-11 13:46:42 +0300

    vue project created and added tailwind css

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-blue-300">
+       <h1 class="text-2xl font-bold underline text-red-700">
+    Hello world!
+  </h1>
+    </div>
+</template>
+
+<script>
+import "tailwindcss/tailwind.css"
+
+export default{
+
+}
+</script>
+
+<style scoped>
+
+</style>
