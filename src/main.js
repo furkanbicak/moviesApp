c04d98579e0aa1c9a52b4efc7a058e5ce90a1fda
afurkanbicak <afurkanbicak@gmail.com>
2022-07-13 14:26:54 +0300

    prototype added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './route/router'
+import api from './services/api'
+
+Vue.prototype.$http = api
 
 new Vue({
   el: '#app',
