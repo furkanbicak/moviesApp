6ef3d50ceed29f652b96d254d5ee3408a3ba0118
afurkanbicak <afurkanbicak@gmail.com>
2022-07-14 11:03:44 +0300

    popular movies

--- src/components/PopularMovies.vue
+++ src/components/PopularMovies.vue
@@ -3,9 +3,9 @@
         <h2 class="uppercase text-yellow-500 text-lg font-semibold">Popular Movies</h2>
 
         <div class="grid grid-cols-4 sm:grid-cols-3 md:cols-4 lg:cols-4 gap-8">
-            <MovieItem />
-            <MovieItem />
-            <MovieItem />
+
+            <MovieItem :key="movie.id" v-for="movie in movies" :movie="movie" :genres="genres"/>
+
         </div>
 
     </div>
@@ -14,9 +14,36 @@
 <script>
 import MovieItem from "./İtems/MovieItem.vue"
 export default {
-    components:{
+        components:{
         MovieItem,
-    }
+    },
+    data(){
+        return{
+            movies: [],
+            genres: [],    
+        }
+    },
+    methods:{
+        async getGenres(){
+            try {
+                const res = await this.$http.get("/genre/movie/list")
+                this.genres = res.data.genres
+                console.log("Genres", this.genres)
+       } catch (error) {
+            console.log("error", error)
+       }
+        }
+    },
+    async mounted(){
+        this.getGenres()
+       try {
+            const res = await this.$http.get("/movie/popular")
+            this.movies = res.data.results
+            console.log("data", this.movies)
+       } catch (error) {
+            console.log("error", error)
+       }
+    },
 }
 </script>
 
